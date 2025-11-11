bc32889192f724ce80ca7f2f1b9f3e28ac5ddb30
Christopher Georg <christopher.georg@sr-travel.de>
2025-11-11 10:49:03 +0100

    feat: allow PHPUnit 10-11

--- src/HttpBaseTest.php
+++ src/HttpBaseTest.php
@@ -36,7 +36,7 @@ abstract class HttpBaseTest extends TestCase
     /**
      * @var array
      */
-    protected $defaultHeaders = [
+    protected static $defaultHeaders = [
         'Connection' => 'close',
         'User-Agent' => 'PHP HTTP Adapter',
         'Content-Length' => '0',
@@ -98,7 +98,7 @@ abstract class HttpBaseTest extends TestCase
         return $cartesianProduct->compute();
     }
 
-    private function getMethods(): array
+    private static function getMethods(): array
     {
         return [
             'GET',
@@ -116,7 +116,7 @@ abstract class HttpBaseTest extends TestCase
      *
      * @return string|null
      */
-    protected function getUri(array $query = [])
+    protected static function getUri(array $query = [])
     {
         return !empty($query)
             ? PHPUnitUtility::getUri().'?'.http_build_query($query, '', '&')
@@ -134,25 +134,25 @@ abstract class HttpBaseTest extends TestCase
     /**
      * @return array
      */
-    private function getUrisAndOutcomes()
+    private static function getUrisAndOutcomes()
     {
         return [
             [
-                $this->getUri(['client_error' => true]),
+                self::getUri(['client_error' => true]),
                 [
                     'statusCode' => 400,
                     'reasonPhrase' => 'Bad Request',
                 ],
             ],
             [
-                $this->getUri(['server_error' => true]),
+                self::getUri(['server_error' => true]),
                 [
                     'statusCode' => 500,
                     'reasonPhrase' => 'Internal Server Error',
                 ],
             ],
             [
-                $this->getUri(['redirect' => true]),
+                self::getUri(['redirect' => true]),
                 [
                     'statusCode' => 302,
                     'reasonPhrase' => 'Found',
@@ -165,7 +165,7 @@ abstract class HttpBaseTest extends TestCase
     /**
      * @return array
      */
-    private function getProtocolVersions()
+    private static function getProtocolVersions()
     {
         return ['1.1', '1.0'];
     }
@@ -173,14 +173,14 @@ abstract class HttpBaseTest extends TestCase
     /**
      * @return string[]
      */
-    private function getHeaders()
+    private static function getHeaders()
     {
-        $headers = $this->defaultHeaders;
+        $headers = self::$defaultHeaders;
         $headers['Accept-Charset'] = 'utf-8';
         $headers['Accept-Language'] = 'en';
 
         return [
-            $this->defaultHeaders,
+            self::$defaultHeaders,
             $headers,
         ];
     }
@@ -188,18 +188,18 @@ abstract class HttpBaseTest extends TestCase
     /**
      * @return array
      */
-    private function getBodies()
+    private static function getBodies()
     {
         return [
             null,
-            http_build_query($this->getData(), '', '&'),
+            http_build_query(self::getData(), '', '&'),
         ];
     }
 
     /**
      * @return array
      */
-    private function getData()
+    private static function getData()
     {
         return ['param1' => 'foo', 'param2' => ['bar', ['baz']]];
     }
@@ -223,7 +223,7 @@ abstract class HttpBaseTest extends TestCase
         if (null === $options['body']) {
             $this->assertEmpty($response->getBody()->__toString());
         } else {
-            $this->assertStringContainsStringCompatible($options['body'], $response->getBody()->__toString());
+            self::assertStringContainsStringCompatible($options['body'], $response->getBody()->__toString());
         }
     }
 
